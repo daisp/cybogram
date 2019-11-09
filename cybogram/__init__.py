61d2ba1920926b54b151efac118edebfed06f647
Dais <daispaysagist@gmail.com>
2019-11-09 10:53:14 +0200

    add logging and demo user config file

--- cybogram/__init__.py
+++ cybogram/__init__.py
@@ -12,7 +12,12 @@ logging.basicConfig(level=logging.INFO)
 
 class Cybogram:
     def __init__(self):
-        logging.info(datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + 'New Cybogram bot instantiation initiated.')
+        self.__time_format = '%Y-%m-%d %H:%M:%S\t'
+        print(
+            '\n\nThank you for using Cybogram! Setup and usage instructions can be found in the README.md file.' +
+            '\nThis is an open source project, the source code is on GitHub: https://github.com/daisp/cybogram\n')
+        time.sleep(0.1)
+        logging.info(datetime.now().strftime(self.__time_format) + '⚙\tInstantiating a Cybogram bot...')
         self.__seconds_to_sleep_after_login = 5
         self.__seconds_to_sleep_after_like = 3
         self.__seconds_to_sleep_after_comment = 21
@@ -21,27 +26,27 @@ class Cybogram:
         self.__browser = webdriver.Firefox()
         self.__browser.implicitly_wait(4)
         logging.info(
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + 'New Cybogram bot instance created successfully.')
+            datetime.now().strftime(self.__time_format) + '✔\tNew Cybogram bot instance created.')
 
     def __load_config_files(self):
         logging.info(
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + 'Attempting to load configuration files from disk.')
+            datetime.now().strftime(self.__time_format) + '⚙\tLoading configuration files from disk...')
         # system settings configuration file
         with open('./config/sys_config.yaml') as sys_config_file:
             self.__sys_config = yaml.full_load(sys_config_file)
             logging.info(
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + 'System configuration file loaded successfully.')
+                datetime.now().strftime(self.__time_format) + '✔\tSystem configuration file loaded.')
 
         # user settings configuration file
         with open('./config/user_config.yaml') as user_config_file:
             self.__user_config = yaml.full_load(user_config_file)
             logging.info(
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + 'User configuration file loaded successfully.')
+                datetime.now().strftime(self.__time_format) + '✔\tUser configuration file loaded.')
 
     def __login(self, account: str):
         self.__account = account
         logging.info(
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + f'Attempting to login using account {self.__account}.')
+            datetime.now().strftime(self.__time_format) + f'⚙\tAttempting to login using account {self.__account}...')
         username_field = self.__browser.find_element_by_xpath('//input[@name=\"username\"]')
         password_field = self.__browser.find_element_by_xpath('//input[@name=\"password\"]')
         login_button = self.__browser.find_element_by_xpath(
@@ -51,7 +56,7 @@ class Cybogram:
         password_field.send_keys(self.__user_config[self.__account]['password'])
         login_button.click()
         logging.info(
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S - ') + f'Successful login using account {self.__account}.')
+            datetime.now().strftime(self.__time_format) + f'🔑\tLogged in using account {self.__account}.')
 
     def __load_random_post_from_random_hashtag(self):
         hashtag_to_search = random.choice(self.__user_config[self.__account]['hashtag_list'])
@@ -64,7 +69,7 @@ class Cybogram:
             'href')
         self.__browser.get(chosen_post_url_suffix)
         logging.info(datetime.now().strftime(
-            '%Y-%m-%d %H:%M:%S - ') + f'Successfully loaded the post {self.__browser.current_url} using the hashtag "{hashtag_to_search}".')
+            self.__time_format) + f'📷\tLoaded:\t{self.__browser.current_url} using the hashtag "{hashtag_to_search}".')
 
     def __run_hashtag_search(self, query: str):
         self.__browser.get(self.__sys_config['ig_explore_hashtag_url'] + query + '/')
@@ -79,13 +84,13 @@ class Cybogram:
                 '..')
             like_button.click()
             logging.info(datetime.now().strftime(
-                '%Y-%m-%d %H:%M:%S - ') + f'Successfully liked the post {self.__browser.current_url}')
+                self.__time_format) + f'❤\tLiked:\t{self.__browser.current_url}')
         except common.exceptions.NoSuchElementException:
             logging.error(datetime.now().strftime(
-                '%Y-%m-%d %H:%M:%S - ') + f'The post {self.__browser.current_url} seems to already be liked by this account.')
+                self.__time_format) + f'❌\tAlready liked:\t{self.__browser.current_url}')
         except:
             logging.error(datetime.now().strftime(
-                '%Y-%m-%d %H:%M:%S - ') + f'An unknown error occurred on the page {self.__browser.current_url}')
+                self.__time_format) + f'❌\tUnknown error on {self.__browser.current_url} while trying to like.')
 
     def __comment_on_current_post(self):
         """
@@ -95,6 +100,21 @@ class Cybogram:
         """
         chosen_comment = random.choice(self.__user_config[self.__account]['comment_list'])
         try:
+            # check if already commented on this post in the past
+            my_own_account_links = self.__browser.find_elements_by_xpath(
+                f'//a[@title=\"{self.__account}\"][@href=\"{self.__account}\"]')
+            logging.info(f'found {len(my_own_account_links)} instances of links to own account in page')
+
+            if len(my_own_account_links) > 0:
+                # already commented on this post
+                logging.info(datetime.now().strftime(
+                    self.__time_format) + f'😞\tAlready commented on{self.__browser.current_url}')
+                return
+        except:
+            logging.error(datetime.now().strftime(
+                self.__time_format) + f'❌\tUnknown error on {self.__browser.current_url} while trying to count the number of my own past comments.')
+
+        try:
             comment_text_field = self.__browser.find_element_by_xpath('//textarea[@aria-label=\"Add a comment…\"]')
             comment_text_field.click()
             comment_text_field = self.__browser.find_element_by_xpath('//textarea[@aria-label=\"Add a comment…\"]')
@@ -102,10 +122,10 @@ class Cybogram:
             comment_text_field.send_keys(Keys.ENTER)
         except common.exceptions.NoSuchElementException:
             logging.error(datetime.now().strftime(
-                '%Y-%m-%d %H:%M:%S - ') + f'Could not find the comment field on the page {self.__browser.current_url}')
+                self.__time_format) + f'❌\tCould not find the comment field on {self.__browser.current_url}')
         except:
             logging.error(datetime.now().strftime(
-                '%Y-%m-%d %H:%M:%S - ') + f'An unknown error occurred on the page {self.__browser.current_url}')
+                self.__time_format) + f'❌\tUnknown error on {self.__browser.current_url} while trying to comment.')
 
     def __follow_random_account_from_post(self):
         raise NotImplementedError
@@ -129,6 +149,8 @@ class Cybogram:
                 time.sleep(self.__seconds_to_sleep_after_like)
             if num_commented < self.__user_config[account]['comments_per_run']:
                 self.__load_random_post_from_random_hashtag()
+                self.__like_current_post()
+                time.sleep(self.__seconds_to_sleep_after_like / 2)
                 self.__comment_on_current_post()
                 num_commented += 1
                 time.sleep(self.__seconds_to_sleep_after_comment)
